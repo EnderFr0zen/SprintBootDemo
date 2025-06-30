6a675894b4fd832b18e04c9debbd7463896c3218
Hsing-Wei (Cynwell) Liao <cynwellliao.20011203@gmail.com>
2025-06-30 18:45:40 +1000

    feat: inject custom application properties

--- src/main/java/com/springboot/demo/SpringBootDemo/rest/RestController.java
+++ src/main/java/com/springboot/demo/SpringBootDemo/rest/RestController.java
@@ -1,9 +1,26 @@
 package com.springboot.demo.SpringBootDemo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
+
+    // inject properties for: firstname.name, preferredname.name and lastname.name
+    @Value("${firstname.name}")
+    private String firstName;
+
+    @Value("${preferredname.name}")
+    private String preferredName;
+
+    @Value("${lastname.name}")
+    private String lastName;
+
+    @GetMapping("/personalinfo")
+    public String getPersonalInfo() {
+        return "Name: " + firstName + " (" + preferredName + ") " + lastName;
+    }
+
     // expose "/" that return "Hello World"
     @GetMapping("/")
     public String helloSpring(){
