f586fecf97c61f7c52af82d9bc55ccd35b6af4f0
Hsing-Wei (Cynwell) Liao <cynwellliao.20011203@gmail.com>
2025-07-04 16:37:47 +1000

    feat: update Spring Security configuration

--- src/main/java/com/springboot/demo/SpringBootDemo/config/SecurityConfig.java
+++ src/main/java/com/springboot/demo/SpringBootDemo/config/SecurityConfig.java
@@ -2,27 +2,61 @@ package com.springboot.demo.SpringBootDemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/v3/api-docs/**",
-                                "/swagger-ui/**"
-                        ).permitAll()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails cynwell = User.builder()
+                .username("cynwell")
+                .password("{noop}123")
+                .roles("ADMIN")
+                .build();
+        UserDetails ninni = User.builder()
+                .username("ninni")
+                .password("{noop}123")
+                .roles("MANAGER")
+                .build();
+        UserDetails inch = User.builder()
+                .username("inch")
+                .password("{noop}123")
+                .roles("EMPLOYEE")
+                .build();
+        return new InMemoryUserDetailsManager(cynwell, ninni, inch);
+    }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        // permit all for Spring Doc
+                        .requestMatchers("/api.docs").permitAll()
+                        .requestMatchers("/api.docs.yaml").permitAll()
+                        // OPTIONS for all public
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        // GET, HEAD, OPTIONS for all Role
+                        .requestMatchers(HttpMethod.GET, "/**").hasAnyRole("EMPLOYEE", "MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.HEAD, "/**").hasAnyRole("EMPLOYEE", "MANAGER", "ADMIN")
+                        // POST, PUT, PATCH for MANAGER and ADMIN
+                        .requestMatchers(HttpMethod.POST, "/**").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/**").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/**").hasAnyRole("MANAGER", "ADMIN")
+                        // DELETE for ADMIN only
+                        .requestMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
+        );
+        // use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+        // disable Cross Site Request Forgery (CSRF)
+        // in general, not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
+        http.csrf(csrf -> csrf.disable());
         return http.build();
     }
 }
