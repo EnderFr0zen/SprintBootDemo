8c96d20d2f4c749fea033fa2c917f162f54657ef
Hsing-Wei (Cynwell) Liao <cynwellliao.20011203@gmail.com>
2025-07-09 17:29:38 +1000

    feat: enhance LoggingAspect to log method signature and arguments using JoinPoint

--- src/main/java/com/springboot/demo/SpringBootDemo/aspect/LoggingAspect.java
+++ src/main/java/com/springboot/demo/SpringBootDemo/aspect/LoggingAspect.java
@@ -1,7 +1,10 @@
 package com.springboot.demo.SpringBootDemo.aspect;
 
+import com.springboot.demo.SpringBootDemo.entity.Account;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -13,8 +16,25 @@ public class LoggingAspect {
     // this is where we add all of our related advices for logging
     // @Before advice
     @Before("com.springboot.demo.SpringBootDemo.aspect.PointcutExpression.forDaoPackage()")
-    public void beforeAdvice() {
+    public void beforeAdvice(JoinPoint joinPoint) {
         System.out.println("\n --- Executing @Before Advice Using Pointcut Declarations on any return type * com.springboot.demo.SpringBootDemo.dao.*.*(..) ---");
+        // display the method signature
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("Logged Method Signature: " + methodSignature);
+        // display method arguments
+        // get args
+        Object[] args = joinPoint.getArgs();
+        // loop through args
+        for (Object arg : args) {
+            System.out.println("Logged Arg: " + arg);
+            if (arg instanceof Account) {
+                // downcast and print Account specific info
+                Account account = (Account) arg;
+                System.out.println("Logged Account Name: " + account.getName());
+                System.out.println("Logged Account Level: " + account.getLevel());
+            }
+        }
+
     }
 
 //    @Before("execution(* com.springboot.demo.SpringBootDemo.dao.*.*(..))")
